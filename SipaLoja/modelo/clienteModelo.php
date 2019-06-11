3fdef1345ba48bb00467611eb44ee9aafb64bf9a
bre-sampaio <47746715+bre-sampaio@users.noreply.github.com>
2019-06-11 12:43:07 -0300

    Add files via upload

---
+++ SipaLoja/modelo/clienteModelo.php
@@ -0,0 +1,75 @@
+<?php
+
+	function adicionarCliente($nome, $email, $senha, $cpf, $sexo, $nascimento){
+		$sql = "INSERT INTO cliente ( nome, 
+                                              email, 
+                                              senha, 
+                                              cpf, 
+					sexo, 
+									  nascimento)
+				VALUES('$nome', 
+						'$senha', 
+						'$email', 
+						'$cpf', 
+						'$sexo', 
+						'$nascimento')";
+
+		$resultado = mysqli_query($cnx = conn(), $sql);
+
+		if(!$resultado) {
+
+			die('erro ao cadastrar cliente <br>' .mysqli_error($cnx));
+		}
+
+		return 'Cadastro realizado com sucesso!';
+	}
+
+
+
+	function pegarTodosClientes(){
+		$sql = "SELECT * FROM cliente";
+		$resultado = mysqli_query(conn(), $sql);
+		$clientes = array();
+		while ($linha = mysqli_fetch_assoc($resultado)) {
+			$clientes[] = $linha;
+		}
+
+		return $clientes;
+	}
+      
+     /* BUSCA CLIENTE PELO ID */   
+        
+        function pegarClientePorId($id){
+            $sql = "SELECT * FROM cliente WHERE idcliente = $id";
+            $resul = mysqli_query(conn(), $sql);
+            $cliente = mysqli_fetch_assoc($resul);
+            return $cliente;
+        }
+        
+     /* DELETAR CLIENTE */   
+        function deletarCliente(){
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
